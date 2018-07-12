a051ea58c78da7e7cb722782c1b93e5dedafe3df
Administrator <2451222478@qq.com>
2018-07-12 19:50:32 +0800

    Initial commit

---
+++ src/main/java/com/dhs/spring_boot_study/SpringBootStudyApplication.java
@@ -0,0 +1,12 @@
+package com.dhs.spring_boot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyApplication.class, args);
+    }
+}
